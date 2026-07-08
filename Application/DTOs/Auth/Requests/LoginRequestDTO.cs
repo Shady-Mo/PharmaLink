b47632f42637363f65d34710aedb475dca576ff1
Eslam Tarek <eslamsulaymaan@gmail.com>
2026-07-08 19:30:47 +0300

    add login DTOS ,jwt token generator and login service interfaces

---
+++ Application/DTOs/Auth/Requests/LoginRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Auth.Requests
+{
+    internal class LoginRequestDTO
+    {
+    }
+}
---
+++ Application/DTOs/Auth/Responses/LoginResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Auth.Responses
+{
+    public class LoginResponseDTO
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public DateTime ExpiresAtUtc { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
