495966d00dabed8ddbc5cebdabe29b628d8c7967
Omar Hany <ohany3051@gmail.com>
2026-07-10 04:21:37 +0300

    add Create & Update for inventory

---
+++ API/Controllers/InventoryController.cs
@@ -0,0 +1,39 @@
+﻿using Application.DTOs.PharmacyInventory.Request;
+using Application.DTOs.PharmacyInventory.Response;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController(IInventoryService inventoryService, IWebHostEnvironment env) : BaseApiController
+    {
+
+        [HttpPost("")]
+        [ProducesResponseType(typeof(PharmacyInventoryDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateDrug(AddPharmacyInventoryDto dto, CancellationToken cancellationToken)
+        {
+            var result = await inventoryService.CreateAsync(dto, cancellationToken);
+
+            return result.IsSuccess
+                ? Created("",result.Value)
+                : result.ToProblem();
+        }
+
+        [HttpPut("")]
+        [ProducesResponseType(typeof(PharmacyInventoryDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateDrug(UpdatePharmacyInventoryDto dto, CancellationToken cancellationToken)
+        {
+            var result = await inventoryService.UpdateAsync(dto, cancellationToken);
+
+            return result.IsSuccess
+                ? Ok(result.Value)
+                : result.ToProblem();
+        }
+    }
+}
---
+++ Application/DTOs/PharmacyInventory/Response/PharmacyInventoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.PharmacyInventory.Response
+{
+    public class PharmacyInventoryDto
+    {
+        public Guid InventoryId { get; set; }
+        public Guid BranchId { get; set; }
+
+        public Guid DrugId { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+        public DateOnly? ExpiryDate { get; set; }
+    }
+}
