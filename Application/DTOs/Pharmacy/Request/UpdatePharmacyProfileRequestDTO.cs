de84a88127bef2c8edd2f7255f54581e0633112f
Omar Hany <ohany3051@gmail.com>
2026-07-15 05:35:14 +0300

    Implement Update Pharmacy Profile Endpoint

---
+++ Application/DTOs/Pharmacy/Request/UpdatePharmacyProfileRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Pharmacy.Request
+{
+    public class UpdatePharmacyProfileRequestDTO
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+    }
+}
---
+++ Application/DTOs/Pharmacy/Responses/UpdatePharmacyProfileResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Pharmacy.Responses
+{
+    public class UpdatePharmacyProfileResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
