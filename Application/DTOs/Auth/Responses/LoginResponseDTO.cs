cca4de55d0d9e9423f51d1a607731d67e13bef2e
Eslam Tarek <eslamsulaymaan@gmail.com>
2026-07-16 18:11:57 +0300

    make login return userId in case of unverified User Phone number

--- Application/DTOs/Auth/Responses/LoginResponseDTO.cs
+++ Application/DTOs/Auth/Responses/LoginResponseDTO.cs
@@ -2,13 +2,22 @@
 {
     public class LoginResponseDTO
     {
-        public string AccessToken { get; set; } = string.Empty;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public string? AccessToken { get; set; }
         public Guid UserId { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 
-        public string FullName { get; set; } = string.Empty;
+        public string? FullName { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 
-        public string Email { get; set; } = string.Empty;
-        public DateTime ExpiresAtUtc { get; set; }
-        public string RoleName { get; set; } = string.Empty;
+        public string? Email { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public DateTime? ExpiresAtUtc { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? RoleName { get; set; }
+        public bool RequiresPhoneVerification { set; get; } = false;
+
     }
 }
