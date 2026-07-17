cb31a6f7e89e49b8b1a9cc0ee5cc41996b68f369
ITI-Student <shady.mohamed7899@gmail.com>
2026-07-17 04:11:28 +0300

    Remove connection testing for redis cache

--- API/Program.cs
+++ API/Program.cs
@@ -11,21 +11,6 @@ builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope()) {
-    var cache = scope.ServiceProvider.GetRequiredService<IDistributedCache>();
-    try {
-        await cache.SetStringAsync("TestKey", "Redis_Is_Connected", new DistributedCacheEntryOptions {
-            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1)
-        });
-
-        var value = await cache.GetStringAsync("TestKey");
-        Console.WriteLine($"[Redis Success]: Connected successfully!\n");
-    }
-    catch (Exception ex) {
-        Console.WriteLine($"[Redis Error]: Connection failed!\n");
-    }
-}
-
 app.UseSwaggerDocs();
 
 app.UseScalarDocs();
