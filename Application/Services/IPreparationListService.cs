7f47136c99df3bbab9a33b1e0458cf9b8873da03
Omar Hany <ohany3051@gmail.com>
2026-07-21 01:00:46 +0300

    Implement Preparation List API

---
+++ Application/Services/IPreparationListService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.PreparationList.Request;
+using Application.DTOs.PreparationList.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    public interface IPreparationListService
+    {
+        Task<Result<PaginatedList<PreparationListDTO>>> GetPreparationListByPharmacistId(Guid id, PreparationListQueryParameters parameters);
+    }
+}
