2a896b0c70f12b7b8e96aef48ce5b38c343cde07
Omar Hany <ohany3051@gmail.com>
2026-07-21 03:58:59 +0300

    update Route

--- API/Controllers/PreparationListController.cs
+++ API/Controllers/PreparationListController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class PreparationListController(IPreparationListService preparationListService)
         : BaseApiController
