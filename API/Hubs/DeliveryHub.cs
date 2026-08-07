eb41f77eb4f1f9e1b05cdd765f99cafcb0856236
Omar Hany <ohany3051@gmail.com>
2026-08-07 05:40:26 +0300

    c2

--- API/Hubs/DeliveryHub.cs
+++ API/Hubs/DeliveryHub.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.SignalR;
 
 namespace API.Hubs
 {
     [Authorize]
-    public class DeliveryHub : Hub
+    public class DeliveryHub(IDeliveryDriverService driverService) : Hub
     {
+        public async Task UpdateLocation(double longitude, double latitude)
+        {
+            var userId = Context.User.GetUserId();
+
+            if (userId != null)
+            {
+                await driverService.UpdateLocationAsync(userId, longitude, latitude);
+            }
+        }
         public override async Task OnConnectedAsync()
         {
             var user = Context.User;
