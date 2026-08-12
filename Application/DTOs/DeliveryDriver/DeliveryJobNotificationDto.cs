b97b97481a7f4d233facba312356761cd72c91d8
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2026-08-13 01:28:25 +0300

    Fix build errors in DeliveryDriverService

--- Application/DTOs/DeliveryDriver/DeliveryJobNotificationDto.cs
+++ Application/DTOs/DeliveryDriver/DeliveryJobNotificationDto.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Application.DTOs.DeliveryDriver;
 
-namespace Application.DTOs.DeliveryDriver
+public class DeliveryJobNotificationDto
 {
-    public class DeliveryJobNotificationDto
-    {
-        public Guid JobId { get; set; }
-        public string PharmacyName { get; set; } = string.Empty;
-        public string FullAddress { get; set; } = string.Empty;
-        public decimal DeliveryFee { get; set; }
-        public double DistanceKm { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public double PharmacyLatitude { get; set; }
-        public double PharmacyLongitude { get; set; }
-    }
+    public Guid JobId { get; set; }
+    public string PharmacyName { get; set; } = string.Empty;
+    public string FullAddress { get; set; } = string.Empty;
+    public decimal DeliveryFee { get; set; }
+    public double DistanceKm { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    public double PharmacyLatitude { get; set; }
+    public double PharmacyLongitude { get; set; }
 }
